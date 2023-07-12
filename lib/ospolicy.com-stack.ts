c3a69f7d324ea5483db6be4a2a77de3db4386f53
Paul <paul@smarterprey.com>
2023-07-12 06:54:46 +0000

    adding ecs cluster

--- lib/ospolicy.com-stack.ts
+++ lib/ospolicy.com-stack.ts
@@ -10,7 +10,9 @@ import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-//import * as widget_service from "./widget_service";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
 
 export class OspolicyComStack extends cdk.Stack {
@@ -147,7 +149,24 @@ export class OspolicyComStack extends cdk.Stack {
       zone
     });
 
-    // create widget service
-    //new widget_service.WidgetService(this, 'WidgetService');
+    //create ecs fargate cluster from ecs_patterns
+    
+    const vpc = new ec2.Vpc(this, "ECSVpc", {
+      maxAzs: 3
+    }); 
+  
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+  
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "GoatService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is true
+    });
   }
 }
