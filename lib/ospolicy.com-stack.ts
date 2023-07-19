e0169bdf6f7c091ef2b471be65fdaae37052c840
Paul Pottorff <8058447+ppottorff@users.noreply.github.com>
2023-07-19 15:26:51 -0700

    Update ospolicy.com-stack.ts
    remove ecs fargate

--- lib/ospolicy.com-stack.ts
+++ lib/ospolicy.com-stack.ts
@@ -149,24 +149,24 @@ export class OspolicyComStack extends cdk.Stack {
       zone
     });
 
-    //create ecs fargate cluster from ecs_patterns
+    // //create ecs fargate cluster from ecs_patterns
     
-    const vpc = new ec2.Vpc(this, "ECSVpc", {
-      maxAzs: 3
-    }); 
+    // const vpc = new ec2.Vpc(this, "ECSVpc", {
+    //   maxAzs: 3
+    // }); 
   
-    const cluster = new ecs.Cluster(this, "MyCluster", {
-      vpc: vpc
-    });
+    // const cluster = new ecs.Cluster(this, "MyCluster", {
+    //   vpc: vpc
+    // });
   
-    // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "GoatService", {
-      cluster: cluster, // Required
-      cpu: 512, // Default is 256
-      desiredCount: 6, // Default is 1
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-      memoryLimitMiB: 2048, // Default is 512
-      publicLoadBalancer: true // Default is true
-    });
+    // // Create a load-balanced Fargate service and make it public
+    // new ecs_patterns.ApplicationLoadBalancedFargateService(this, "GoatService", {
+    //   cluster: cluster, // Required
+    //   cpu: 512, // Default is 256
+    //   desiredCount: 6, // Default is 1
+    //   taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+    //   memoryLimitMiB: 2048, // Default is 512
+    //   publicLoadBalancer: true // Default is true
+    // });
   }
 }
